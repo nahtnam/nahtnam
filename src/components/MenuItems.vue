f86745a6880aeeea5f49b531834e0b8bc0fadbd7
Manthan Mallikarjun <nahtnam@gmail.com>
2018-08-17 22:43:25 -0700

    add blog to menu

--- src/components/MenuItems.vue
+++ src/components/MenuItems.vue
@@ -3,5 +3,6 @@
     <router-link to="/" class="navbar-item" active-class="is-active" exact>Home</router-link>
     <router-link to="/projects" class="navbar-item" active-class="is-active">Projects</router-link>
     <router-link to="/photos" class="navbar-item" active-class="is-active">Photos</router-link>
+    <a href="https://blog.nahtnam.com" class="navbar-item">Blog</a>
   </div>
 </template>
