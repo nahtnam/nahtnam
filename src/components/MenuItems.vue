ef422dcc5bdb94d6e5d78f9204b9d86c196605c1
Manthan Mallikarjun <nahtnam@gmail.com>
2018-08-17 22:42:05 -0700

    add photos page

--- src/components/MenuItems.vue
+++ src/components/MenuItems.vue
@@ -2,5 +2,6 @@
   <div>
     <router-link to="/" class="navbar-item" active-class="is-active" exact>Home</router-link>
     <router-link to="/projects" class="navbar-item" active-class="is-active">Projects</router-link>
+    <router-link to="/photos" class="navbar-item" active-class="is-active">Photos</router-link>
   </div>
 </template>
