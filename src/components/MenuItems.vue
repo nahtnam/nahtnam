50cbac6ec7a8b4a749dfe7e43d37e016d71ebc29
Manthan Mallikarjun <nahtnam@gmail.com>
2018-08-17 22:49:34 -0700

    add link to resume

--- src/components/MenuItems.vue
+++ src/components/MenuItems.vue
@@ -4,5 +4,6 @@
     <router-link to="/projects" class="navbar-item" active-class="is-active">Projects</router-link>
     <router-link to="/photos" class="navbar-item" active-class="is-active">Photos</router-link>
     <a href="https://blog.nahtnam.com" class="navbar-item">Blog</a>
+    <a href="/resume.pdf" class="navbar-item">Résumé</a>
   </div>
 </template>
