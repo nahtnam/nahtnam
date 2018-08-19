3107fec47727b06b675b6b741a02c7ee058c81c1
Manthan Mallikarjun <nahtnam@gmail.com>
2018-08-19 15:00:57 -0700

    add google analytics

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 
+import VueAnalytics from 'vue-analytics'
+
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCode, faCamera } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
@@ -10,8 +12,11 @@ import router from './router';
 import store from './store';
 
 library.add(fab, faCode, faCamera);
-
 Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.use(VueAnalytics, {
+  id: 'UA-124193870-1',
+});
 
 Vue.config.productionTip = false;
 
