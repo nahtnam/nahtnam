095f12e579692974fe14b7e2df0a4a0c4bba8011
Manthan Mallikarjun <nahtnam@gmail.com>
2018-08-19 16:46:45 -0700

    update home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,7 +25,7 @@
                   but I'm known online as <a href="https://keybase.io/nahtnam" target="_blank">@nahtnam</a>.
                   I enjoy programming in languages like <strong>JavaScript</strong> (<strong>Node</strong>), and <strong>Ruby</strong>, exploring frameworks like <strong>Vue</strong>, and researching emerging technology like <strong>Bitcoin</strong>.
                   I also take photos when I find the time.
-                  I am currently working on a few projects, incluidng <strong><a href="https://konstructor.js.org/" target="_blank">konstructor</a></strong>, and <strong><a href="https://github.com/konstructorjs/kolony" target="_blank">kolony</a></strong>, as well as conducting research on cryptocurrencies like <strong>Ethereum</strong> at UC Santa Cruz.
+                  I am currently working on a few projects, incluidng <strong><a href="https://github.com/srvrjs/srvr" target="_blank">srvr</a></strong>, and <strong><a href="https://github.com/ludicrousxyz/tab" target="_blank">ludicrous tab</a></strong>, as well as conducting research on cryptocurrencies like <strong>Ethereum</strong> at UC Santa Cruz.
               </h2>
             </div>
             <div class="column is-one-third">
