1c850581e868cc3657dfec3199c720be20e2de90
Manthan Mallikarjun <nahtnam@gmail.com>
2018-08-29 23:58:03 -0700

    install nuxt

---
+++ components/Navigation.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="navbar is-white has-shadow" id="main-navbar">
+    <div class="container">
+      <div class="navbar-brand">
+        <router-link class="navbar-item has-text-weight-bold" to="/">
+          <span class="is-size-5">Fight FUSD</span>
+        </router-link>
+
+        <div class="navbar-burger burger" :class="{ 'is-active': active }" @click="toggleMenu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+
+      <div class="navbar-menu" :class="{ 'is-active': active }">
+        <div class="navbar-end">
+          <nuxt-link to="/" class="navbar-item" active-class="is-active" @click.native="closeMenu" exact>Home</nuxt-link>
+          <nuxt-link to="/qanda" class="navbar-item" active-class="is-active" @click.native="closeMenu">Q and A</nuxt-link>
+          <nuxt-link to="/contact" class="navbar-item" active-class="is-active" @click.native="closeMenu">Contact</nuxt-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data() {
+    return {
+      active: false,
+    };
+  },
+  methods: {
+    closeMenu() {
+      this.active = false;
+    },
+    toggleMenu() {
+      this.active = !this.active;
+    },
+  },
+};
+</script>
