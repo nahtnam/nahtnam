5565f3395d3eea41b4d9cc92bfc8f9587a639382
Manthan Mallikarjun <nahtnam@gmail.com>
2019-02-25 18:27:11 -0800

    update timeline to be dynamic based on a JSON file

--- src/components/resume/Timeline.vue
+++ src/components/resume/Timeline.vue
@@ -1,76 +1,41 @@
 <template>
-  <div class="timeline is-rtl">
+  <div class="timeline">
     <header class="timeline-header">
-      <span class="tag is-medium is-primary">{{ this.year + 1 }}</span>
+      <span class="tag is-medium is-primary">{{ year + 1 }}</span>
     </header>
-    <div class="timeline-item is-success">
-      <div class="timeline-marker is-success is-icon has-text-white">
-        <font-awesome-icon icon="bicycle" />
-      </div>
-      <div class="timeline-content">
-        <p class="heading">
-          <strong>Lime</strong> - Software Engineering Intern
-          <br>
-          <i>FUTURE (2019)</i>
-        </p>
+    <div v-for="item in layout" :key="item.text || item.company">
+      <header class="timeline-header" v-if="item.type === 'tag'">
+        <span class="tag" :class="[item.color, item.size].join(' ')">{{ item.text }}</span>
+      </header>
+      <div class="timeline-item" :class="item.color" v-else-if="item.type === 'item'">
+        <div class="timeline-marker is-icon has-text-white" :class="item.color">
+          <font-awesome-icon :icon="item.icon" />
+        </div>
+        <div class="timeline-content">
+          <p class="heading">
+            <strong>{{ item.company }}</strong> - {{ item.role }}
+            <br>
+            <i v-if="Date.now() < (new Date(item.startDate)).getTime()"><strong>FUTURE</strong> ({{ item.startDate }})</i>
+            <i v-else>
+              {{ item.startDate }} -
+              <span v-if="item.endDate">{{ item.endDate }}</span>
+              <span v-else><strong>Now</strong></span>
+            </i>
+          </p>
+        </div>
       </div>
     </div>
-    <header class="timeline-header">
-      <span class="tag is-primary">2019</span>
-    </header>
-    <div class="timeline-item is-danger">
-      <div class="timeline-marker is-danger is-icon has-text-white">
-        <font-awesome-icon icon="shopping-bag" />
-      </div>
-      <div class="timeline-content">
-        <p class="heading">
-          <strong>Ebates/Rakuten</strong> - Software Engineering Intern
-          <br>
-          <i>June 2015 - September 2018</i>
-        </p>
-      </div>
-    </div>
-    <header class="timeline-header">
-      <span class="tag is-primary">2018</span>
-    </header>
-    <div class="timeline-item is-warning">
-      <div class="timeline-marker is-warning is-icon">
-        <font-awesome-icon icon="shipping-fast" />
-      </div>
-      <div class="timeline-content">
-        <p class="heading">
-          <strong>Roambee</strong> - Software Engineering Intern
-          <br>
-          <i>July 2016 - August 2015</i>
-        </p>
-      </div>
-    </div>
-    <header class="timeline-header">
-      <span class="tag is-primary">2016</span>
-    </header>
-    <div class="timeline-item color-bitcoin">
-      <div class="timeline-marker color-bitcoin is-icon">
-        <font-awesome-icon :icon="['fab', 'bitcoin']" />
-      </div>
-      <div class="timeline-content">
-        <p class="heading">
-          <strong>Card for Coin</strong> - Software Engineering Intern
-          <br>
-          <i>June 2015 - August 2015</i>
-        </p>
-      </div>
-    </div>
-    <header class="timeline-header">
-      <span class="tag is-medium is-primary">2015</span>
-    </header>
   </div>
 </template>
 
 <script>
+import { layout } from '@/static/timeline.json';
+
 export default {
   data() {
     return {
       year: (new Date()).getFullYear(),
+      layout,
     };
   },
 };
@@ -85,6 +50,10 @@ export default {
 .timeline .timeline-item .timeline-marker.color-bitcoin.is-icon {
   border-color: #ff9900;
   background-color: #ff9900;
-  color: white;
+  color: #ffffff;
+}
+.tag:not(body).color-bitcoin {
+  background-color: #ff9900;
+  color: #ffffff;
 }
 </style>
--- src/plugins/font-awesome.js
+++ src/plugins/font-awesome.js
@@ -6,9 +6,10 @@ import {
   faShippingFast,
   faShoppingBag,
   faBicycle,
+  faGraduationCap,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-library.add(faBitcoin, faShippingFast, faShoppingBag, faBicycle);
+library.add(faBitcoin, faShippingFast, faShoppingBag, faBicycle, faGraduationCap);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
