744a605abd6ce560ec1b7530ece14ab09f3c1614
Manthan Mallikarjun <nahtnam@gmail.com>
2019-02-27 23:20:52 -0800

    create is-loading attribute

--- src/components/home/Statistics.vue
+++ src/components/home/Statistics.vue
@@ -3,8 +3,8 @@
     <nav class="level">
       <div class="level-item has-text-centered">
         <div>
-          <p class="title">
-            TODO
+          <p class="title" :class="{ 'is-loading': contributions.loading }">
+            {{ contributions.text }}
           </p>
           <p class="heading">
             Contributions
@@ -13,17 +13,17 @@
       </div>
       <div class="level-item has-text-centered">
         <div>
-          <p class="title">
+          <p class="title" :class="{ 'is-loading': contributions.loading }">
             TODO
           </p>
           <p class="heading">
-            Posts
+            Posts Online
           </p>
         </div>
       </div>
       <div class="level-item has-text-centered">
         <div>
-          <p class="title">
+          <p class="title" :class="{ 'is-loading': contributions.loading }">
             TODO
           </p>
           <p class="heading">
@@ -33,11 +33,11 @@
       </div>
       <div class="level-item has-text-centered">
         <div>
-          <p class="title">
-            {{ age }}
+          <p class="title" :class="{ 'is-loading': age.loading }">
+            {{ age.text }}
           </p>
           <p class="heading">
-            Age
+            Years Old
           </p>
         </div>
       </div>
@@ -49,13 +49,32 @@
 export default {
   data() {
     return {
-      age: 0,
+      age: {
+        text: '---',
+        loading: true,
+      },
+      contributions: {
+        text: '---',
+        loading: true,
+      },
     };
   },
 
-  created() {
-    this.getAge();
-    this.ageInterval = setInterval(this.getAge, 1000);
+  async created() {
+    try {
+      this.getAge();
+      this.ageInterval = setInterval(this.getAge, 1000);
+    } catch (_) {
+      // do nothing
+    }
+    this.age.loading = false;
+
+    try {
+      await this.getContributions();
+    } catch (_) {
+      // do nothing
+    }
+    this.contributions.loading = false;
   },
 
   beforeDestroy() {
@@ -66,10 +85,17 @@ export default {
   },
 
   methods: {
+    async getContributions() {
+      const req = await fetch('https://github-contributions-api.now.sh/v1/nahtnam');
+      const { years } = await req.json();
+      const total = years.reduce((acc, val) => acc + val.total, 0);
+      this.contributions.text = total.toLocaleString();
+    },
+
     getAge(birthday = 'October 10, 1999') {
       const diff = Date.now() - (new Date(birthday).getTime());
       const age = diff / (1000 * 60 * 60 * 24 * 365);
-      this.age = age.toFixed(7);
+      this.age.text = age.toFixed(7);
     },
   },
 };
