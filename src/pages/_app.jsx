abad9de74c6bd550a244a99a85748a73de18794c
Manthan Mallikarjun <nahtnam@gmail.com>
2019-03-25 14:24:39 -0700

    working on porting application from vue

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
 import App, { Container } from 'next/app';
+
 import Navigation from '../components/Navigation';
 import Footer from '../components/Footer';
 
 import '../assets/css/index.scss';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faBitcoin,
+  faGithub,
+  faTwitter,
+  faInstagram,
+  faFacebookSquare,
+  faLinkedin,
+  faStackOverflow,
+  faSteam,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faShippingFast,
+  faShoppingBag,
+  faBicycle,
+  faGraduationCap,
+  faCode,
+  faEye,
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+  faBitcoin,
+  faShippingFast,
+  faShoppingBag,
+  faBicycle,
+  faGraduationCap,
+  faGithub,
+  faTwitter,
+  faInstagram,
+  faFacebookSquare,
+  faLinkedin,
+  faStackOverflow,
+  faSteam,
+  faCode,
+  faEye,
+)
 
 export default class extends App {
   render() {
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,81 @@
 import React from 'react';
 
+import AboutMe from '../components/index/AboutMe';
+import Quote from '../components/index/Quote';
+
+import Timeline from '../components/index/Timeline';
+
 export default () => (
   <div>
-      hello world
+    <section className="hero is-medium is-black">
+      <div className="hero-body">
+        <div className="container has-text-centered">
+          <h1 className="header title">
+            Hi, I'm Manthan.
+          </h1>
+          <h2 className="header subtitle is-6 has-text-weight-light">
+            You can find me online <a href="https://keybase.io/nahtnam" className="has-text-weight-bold is-lowercase" target="_blank" rel="noopener noreferrer">@nahtnam</a>.
+          </h2>
+        </div>
+      </div>
+    </section>
+    <section id="first-section" className="section">
+      <div className="container">
+        <div className="columns">
+          <div className="column no-pad-bottom is-three-quarters">
+            <div className="has-text-centered">
+              <h1 className="header title is-4">
+                About Me
+              </h1>
+              <AboutMe />
+            </div>
+            <hr />
+            <div className="has-text-centered">
+              <h1 className="header title is-4">
+                Statistics
+              </h1>
+              {/* <Statistics /> */}
+            </div>
+            <hr />
+            <div className="has-text-centered">
+              <h1 className="header title is-4">
+                Projects
+              </h1>
+              {/* <Projects /> */}
+            </div>
+            <hr />
+            <div className="has-text-centered">
+              <h1 className="header title is-4">
+                Social Media
+              </h1>
+              {/* <Social /> */}
+            </div>
+            <hr />
+            <div className="has-text-centered">
+              <h1 className="header title is-4">
+                Shout-Outs
+              </h1>
+              <Quote />
+            </div>
+          </div>
+          <div className="column no-pad-bottom is-one-quarter">
+            <hr className="is-hidden-tablet" />
+            <div>
+              <h1 className="header title is-4 has-text-centered">
+                Work
+              </h1>
+              <Timeline />
+            </div>
+            <hr />
+            <div className="has-text-centered">
+              <h1 className="header title is-4">
+                Experience
+              </h1>
+              {/* <Languages /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 );
