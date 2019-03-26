eda2bbdcb8bf27313fa0d22577886afe3a66238b
Manthan Mallikarjun <nahtnam@gmail.com>
2019-03-26 16:04:05 -0700

    update charset

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -51,7 +51,7 @@ export default class extends App {
       <Container>
         <Head>
           <title>nahtnam</title>
-          <meta charset="utf-8" />
+          <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="description" hid="description" content="my personal portfolio." />
           <link rel="icon" type="image/png" href="/static/favicon.png" />
