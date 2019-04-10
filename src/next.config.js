f3263ba8d07fa4cfb1159b38d8ed30fda1309d60
Manthan Mallikarjun <nahtnam@gmail.com>
2019-04-09 17:07:05 -0700

    copy resume over to build

--- src/next.config.js
+++ src/next.config.js
@@ -1,3 +1,23 @@
+const fs = require('fs');
+const { join } = require('path');
+const { promisify } = require('util');
 const withSass = require('@zeit/next-sass');
 
-module.exports = withSass();
+const copyFile = promisify(fs.copyFile);
+
+module.exports = withSass({
+  exportPathMap: async (
+    defaultPathMap,
+    {
+      dev,
+      dir,
+      outDir,
+    },
+  ) => {
+    if (dev) {
+      return defaultPathMap;
+    }
+    await copyFile(join(dir, 'static/resume.pdf'), join(outDir, 'resume.pdf'));
+    return defaultPathMap;
+  },
+});
