64429fb0f16d1671cc09abf00266b6bb913d0b08
Manthan Mallikarjun <nahtnam@gmail.com>
2024-06-24 00:36:16 -0700

    resolve issues

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from 'next';
-import { baseUrl } from '@/config/app';
-import { Footer } from './_components/footer';
-import { Navigation } from './_components/navigation';
+import { baseUrl } from "@/config/app";
+import type { Metadata } from "next";
+import { Footer } from "./_components/footer";
+import { Navigation } from "./_components/navigation";
 
 export const metadata: Metadata = {
-  title: '@nahtnam - Manthan Mallikarjun',
+  title: "@nahtnam - Manthan Mallikarjun",
   description:
-    'Hello! I’m Manthan You can find me online @nahtnam. This is my portfolio where you can read about me and my thoughts.',
+    "Hello! I’m Manthan You can find me online @nahtnam. This is my portfolio where you can read about me and my thoughts.",
   metadataBase: new URL(baseUrl),
 };
 
