2b9370e3490be82c3b40f781384a4f80013168d1
Manthan Mallikarjun <me@nahtnam.com>
2026-06-14 19:43:25 -0700

    Configure WorkOS redirect in AuthKit session

--- src/start.ts
+++ src/start.ts
@@ -1,12 +1,19 @@
 import { createStart } from "@tanstack/react-start";
+import { configure } from "@workos/authkit-session";
 import { authkitMiddleware } from "@workos/authkit-tanstack-react-start";
 import { appUrl } from "./lib/config";
 import { serverEnv } from "./lib/config/server";
 
+const authkitRedirectUri = new URL("/api/auth/callback", appUrl).toString();
+
+configure({
+  redirectUri: authkitRedirectUri,
+});
+
 export const startInstance = createStart(() => ({
   requestMiddleware: [
     authkitMiddleware({
-      redirectUri: new URL("/api/auth/callback", appUrl).toString(),
+      redirectUri: authkitRedirectUri,
     }),
   ],
 }));
